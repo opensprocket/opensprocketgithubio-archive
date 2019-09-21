bda6071315612fbb3577e7c98fb53d7c60caf444
opensprocket <opensprocket@users.noreply.github.com>
2019-09-21 11:49:17 -0600

    fixed syntax errors

--- js/on-page-load.js
+++ js/on-page-load.js
@@ -1,14 +1,13 @@
 //input: page load
 //operation: get date, parse and save date. get last modified, save
 //output: display after page load
-function onPageLoad () {
+function onPageLoad() {
     //update copyright date
     var todaysDate = new Date();
     var currentYear = todaysDate.getFullYear();
     document.getElementById('copyright-year').innerHTML = currentYear;
     //get document.lastmodified
-    var lastModifiedDate = document.lastModified; 
-    document.getElementById('lastupdated').innerHTML = lastModifiedDate;
+    document.getElementById('lastupdated').innerHTML = document.lastModified;
 }
 //calls funciton after page has loaded
 window.onload = onPageLoad();
