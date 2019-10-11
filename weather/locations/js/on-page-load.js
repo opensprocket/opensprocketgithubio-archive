2accb600f89228662d55066da3e8b06999fb1736
opensprocket <opensprocket@users.noreply.github.com>
2019-10-10 23:57:26 -0600

    updated menu styling

--- weather/locations/js/on-page-load.js
+++ weather/locations/js/on-page-load.js
@@ -1,13 +1,19 @@
-function onPageLoad() {
-    //update copyright date
+//prints current date (day name, day  month, year)
+function currentDate() {
+    var options = { weekday: "long", year: "numeric", month: "long", day: "numeric", };
+    var todaysDate = new Date();
+    document.getElementById("current-date").innerHTML = todaysDate.toLocaleDateString("en-US", options);
+}
+//gets the curreent year and displays the copyright info
+function copyrightYear() {
     var todaysDate = new Date();
     var currentYear = todaysDate.getFullYear();
-    document.getElementById('copyright-year').innerHTML = currentYear;
-    //get document.lastmodified
-    document.getElementById('lastupdated').innerHTML = "Last updated: " + document.lastModified;
+    document.getElementById('copyright-year').innerHTML = "&copy; " + currentYear;
 }
-//calls funciton after page has loaded
-window.onload = onPageLoad();
+
+//calls the listed funciton after page has loaded
+window.onload = currentDate();
+window.onload = copyrightYear();
 
 //toggles the mobile hamburger menu open and closed
 function toggleMenu() {
