cb9273ced3a6f1a168bcdcb39f558a4ba8707ef3
opensprocket <opensprocket@users.noreply.github.com>
2019-10-10 20:58:54 -0600

    started building preston page

---
+++ weather/locations/js/on-page-load.js
@@ -0,0 +1,13 @@
+//input: page load
+//operation: get date, parse and save date. get last modified, save
+//output: display after page load
+function onPageLoad() {
+    //update copyright date
+    var todaysDate = new Date();
+    var currentYear = todaysDate.getFullYear();
+    document.getElementById('copyright-year').innerHTML = currentYear;
+    //get document.lastmodified
+    document.getElementById('lastupdated').innerHTML = "This document was last updated: " + document.lastModified;
+}
+//calls funciton after page has loaded
+window.onload = onPageLoad();
