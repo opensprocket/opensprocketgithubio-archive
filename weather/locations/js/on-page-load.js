f60d5d23cd981df2098ca066cdab9eda0b442da2
opensprocket <opensprocket@users.noreply.github.com>
2019-10-19 23:19:18 -0600

    added last updated js

--- weather/locations/js/on-page-load.js
+++ weather/locations/js/on-page-load.js
@@ -14,9 +14,18 @@ function copyrightYear() {
     document.getElementById('copyright-year').innerHTML = "&copy; " + currentYear;
 }
 
+function lastModified() {
+    var modDate = new Date(document.lastModified);
+    const longDayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var dayName = longDayNames[modDate.getDay()];
+    const longMonthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var monthName = longMonthNames[modDate.getMonth()];
+    document.getElementById("last-modified").innerHTML = "Updated: " + dayName + ", " + modDate.getDate() + " " + monthName + " " + modDate.getFullYear();
+}
 //calls the listed funciton after page has loaded
 window.onload = currentDate();
 window.onload = copyrightYear();
+window.onload = lastModified();
 
 //toggles the mobile hamburger menu open and closed
 function toggleMenu() {
