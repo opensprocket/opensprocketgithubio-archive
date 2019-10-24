6ca649f01c0b0b6dc6bd4261487613c39ff6eff4
opensprocket <opensprocket@users.noreply.github.com>
2019-10-23 18:07:42 -0600

    added script for swapping placeholders

---
+++ weather/docs/js/lazy-loading-min.js
@@ -0,0 +1,25 @@
+const images = document.querySelectorAll("img[data-src]");
+function preloadImage(img) {
+  const src = img.getAttribute("data-src");
+  if (!src) {
+    return; 
+  }
+  img.src = src; 
+}
+const imgOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px 50px 0px"
+};
+const imgObserver = new IntersectionObserver((entries, imgObserver)) => {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      preloadImage(entry.target);
+      imgObserver.unobserve(entry.target);
+    }
+  });
+};
+images.forEach(image => {
+  imgObserver.observe(image);
+});
