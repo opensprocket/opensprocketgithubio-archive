7fda062cc7663638328ddbee802040bb289cc13f
opensprocket <opensprocket@users.noreply.github.com>
2019-12-07 21:16:35 -0700

    added thank you for contacting page

--- temple-inn/js/functions.js
+++ temple-inn/js/functions.js
@@ -69,6 +69,9 @@ function buildPageTitle(){
     case "temples":
       title = "Temples";
       break;
+    case "thanks":
+      title = "Thanks"
+      break;
     default:
       title = "New Page";
       break;
